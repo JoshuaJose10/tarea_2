069723728fa44f114a74bbcb7f539b4459b4d17c
JoshuaJose10 <jjdavalos10@gmail.com>
2024-02-21 23:49:20 -0600

    Se modificaron el nombre de las variables

--- Triangulo_de_pascal/src/main/java/com/mycompany/triangulo_de_pascal/Triangulo_de_pascal.java
+++ Triangulo_de_pascal/src/main/java/com/mycompany/triangulo_de_pascal/Triangulo_de_pascal.java
@@ -1,23 +1,19 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.triangulo_de_pascal;
 import java.util.*;
 public class Triangulo_de_pascal {
 
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
-         int n,f;
+         int numero,filas;
         System.out.print("Ingrese el numero de filas: ");
-        f = sc.nextInt();  
+        filas = sc.nextInt();  
         
-        for(int i =0;i<f;i++) {
-            n = 1;
-            System.out.printf("%"+(f-i)*2+"s","");
+        for(int i =0;i<filas;i++) {
+            numero = 1;
+            System.out.printf("%"+(filas-i)*2+"s","");
             for(int j=0;j<=i;j++) {
-                System.out.format("%4d",n);
-                n = n * (i - j) / (j + 1);
+                System.out.format("%4d",numero);
+                numero = numero * (i - j) / (j + 1);
             }
             System.out.println();
         }
